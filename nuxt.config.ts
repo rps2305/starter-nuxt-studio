ee9aadd7049530543b375b8b827c61c2c60475a6
Ronald Punt <ronald.punt@bamfestival.nl>
2024-07-03 13:11:30 +0200

    tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', "@nuxt/image", "@nuxtjs/tailwindcss", 'nuxt-headlessui'],
   routeRules: {
     '/': { prerender: true }
   }
